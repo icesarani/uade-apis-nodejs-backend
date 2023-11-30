2dabad6fa9da2e4dd6c43b56d9312fe969ac204e
Ignacio Cesarani <ignaciocesarani2001@gmail.com>
2023-11-30 16:37:44 -0300

    mensaje de commit

--- routes/api/mentor.route.js
+++ routes/api/mentor.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const MentorController = require("../../controllers/mentors.controller");
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Authorize each API with middleware and map to the Controller Functions
